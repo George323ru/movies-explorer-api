3c0ed28168421f6b05a53aef77c868d09e950ffd
Georgiy Gusev <gus3225@yandex.ru>
2021-09-21 17:31:00 +0500

    fix: add new string to movies

--- models/movie.js
+++ models/movie.js
@@ -1,32 +1,30 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
-  },
   country: {
     type: String,
     required: true,
+    minlength: 2,
   },
   director: {
     type: String,
     required: true,
-
+    minlength: 2,
   },
   duration: {
     type: Number,
     required: true,
-
   },
   year: {
     type: String,
     required: true,
+    minlength: 4,
+    maxlength: 4,
   },
   description: {
     type: String,
     required: true,
+    minlength: 2,
   },
   image: {
     type: String,
@@ -54,15 +52,21 @@ const movieSchema = new mongoose.Schema({
   },
   movieId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
     required: true,
   },
   nameRU: {
     type: String,
     required: true,
+    minlength: 2,
   },
   nameEN: {
     type: String,
+    required: true,
+    minlength: 2,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
     required: true,
   },
 });
